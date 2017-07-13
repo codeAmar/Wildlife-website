21f001aa34e79383e1f9daa480979eeffed1b51c
Amarjot singh <amarjotsingh90@yahoo.com>
2017-07-13 14:17:02 -0700

    introduced promise in thanks POST route

--- app/routes/thanks.js
+++ app/routes/thanks.js
@@ -8,19 +8,21 @@ const wellknown = require('nodemailer-wellknown')
 
 
 var smtpTransport = nodemailer.createTransport({
-    service: "gmail",
+  service: "gmail",
   host: "smtp.gmail.com",
   auth: {
-      user: "creativetechlangara",
-      pass: "Project2"
+    user: "creativetechlangara",
+    pass: "Project2"
   }
 });
 
 
-var template = new EmailTemplate(path.join(__dirname, '../views/partials/template/welcome-email'),{juiceOptions:{
-  preserveMediaQueries: false,
-  removeStyleTags: false
-}})
+var template = new EmailTemplate(path.join(__dirname, '../views/partials/template/welcome-email'), {
+  juiceOptions: {
+    preserveMediaQueries: false,
+    removeStyleTags: false
+  }
+})
 
 
 
@@ -32,51 +34,61 @@ var locals = {
   }
 }
 
+var mailDelivered = false;
 
-
-router.post('/thanks',function(req,res){
+router.post('/thanks', function(req, res) {
   console.log(smtpTransport);
-   
-template.render(locals, function (err, results) {
-  if (err) {
-    return console.error("error while rendering the template "+err)
-  }
-
-  smtpTransport.sendMail({
-    // from: 'Spicy Meatball <spicy.meatball@spaghetti.com>',
-    to: locals.email,
-    subject: results.subject,
-    html: results.html,
-    text: results.text,
-    attachments:[{
-      filename:'image.jpg',
-      path:'app/views/partials/template/welcome-email/image.jpg',
-      cid:'uniqueMailImage'
-    }]
-  }, function (err, responseStatus) {
-
-    if(error){
-      var mailDelivered =false;
-    }else{
-      var mailDelivered = true;
-      console.log("mail is delivered");
-    }
-
-      res.render('thanks',{
-        siteTitle:'thanks',
-        name: req.body.firstName,
-        email: req.body.Email,
-        delivered : mailDelivered });
+deliverMail().then(function(mailDelivered){
+  res.render('thanks', {
+    siteTitle: 'thanks',
+    name: req.body.firstName,
+    email: req.body.Email,
+    delivered: mailDelivered
   })
-})
+}).catch(function(error){res.render('thanks', {
+    siteTitle: 'thanks',
+    name: req.body.firstName,
+    email: req.body.Email,
+    delivered:error
+  })});
+});
 
+router.get('/thanks', function(req, res) {
+  res.render('thanks', {
+    siteTitle: 'addressss'
+  });
 });
 
 
-router.get('/thanks',function(req,res){
-  res.render('thanks',{siteTitle : addressss });
-});
 
 
+function deliverMail(){
+  return new Promise((resolve,reject)=>{
+    template.render(locals, function(err, results) {
+      if (err) {
+        return console.error("error while rendering the template " + err)
+        reject(err); return;
+      }
 
-module.exports= router;
+      smtpTransport.sendMail({
+        to: locals.email,
+        subject: results.subject,
+        html: results.html,
+        text: results.text,
+        attachments: [{
+          filename: 'image.jpg',
+          path: 'app/views/partials/template/welcome-email/image.jpg',
+          cid: 'uniqueMailImage'
+        }]
+      }, function(err, responseStatus) {
+        if (err) {
+          reject(err); return;
+        } else {
+          resolve(true);
+        }
+      })
+    });
+  })
+}
+
+module.exports = router;
