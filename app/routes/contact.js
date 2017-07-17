afd09375c243a203ee0219e6993a9ad5c98a71dd
Amarjot singh <amarjotsingh90@yahoo.com>
2017-07-16 17:49:43 -0700

    contact form changes

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -4,6 +4,26 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require("nodemailer");
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URL,function(err){
+  if(err){
+    console.log("error occurred while connecting to database");
+  }else{
+    console.log('connected to server');
+  }
+});
+
+
+var contactSchema = mongoose.Schema({
+  email:String,
+  name:String,
+  phone:Number,
+  message:String
+});
+
+var contact = mongoose.model('contact',contactSchema);
+
 
 router.get('/contact',function(req,res){
   res.render('contact',{siteTitle:'contact'});
@@ -39,6 +59,36 @@ router.get('/contact/send',function(req,res){
         res.end("sent");
          }
 });
+
+
+    var currentCommenter = new contact({
+      email:req.query.email,
+      name:req.query.name,
+      phone:req.query.phone,
+      message:req.query.message
+
+    });
+
+    contact.find({},function(err,data){
+      console.log("finding all users inside db :" + data +"====================");
+    });
+
+
+console.log('found all ');
+    contact.findOne({message:currentCommenter.message},function(err,data){
+      if(!data){
+        currentCommenter.save(function(err,data){
+          if(err){
+            return cb(err);
+          }
+          cb(null,currentCommenter);
+        });
+      }else{
+        cb(null,data);
+      }
+    });
+
+
 });
 
 module.exports= router;
